91e1fd121be861b5e4dc14edb9b83feeda700955
bhavik3184 <bhavik3184@gmail.com>
2025-05-19 17:00:42 +0530

    Setting up navigation for discover & offers

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,7 +11,7 @@ import { PlacesPage } from './places.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    // FormsModule,
     IonicModule,
     PlacesPageRoutingModule
   ],
